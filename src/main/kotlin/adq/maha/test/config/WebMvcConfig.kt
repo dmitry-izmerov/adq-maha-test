53a640b48c1dca4de8fe6b39919a24d09361bcbb
dmitry-izmerov <idd90y@yandex.ru>
2021-04-21 21:46:08 +0200

    init project

---
+++ src/main/kotlin/adq/maha/test/config/WebMvcConfig.kt
@@ -0,0 +1,15 @@
+package adq.maha.test.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+}
---
+++ src/test/kotlin/adq/maha/test/TestApplicationTests.kt
@@ -0,0 +1,13 @@
+package adq.maha.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
